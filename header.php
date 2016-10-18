ceee3dac77368b75d6e0a1e893bb8d26859c1a8e
Brian <Brian@Brians-MacBook-Pro.local>
2016-10-18 09:08:33 -0400

    added bootstrap/front page

--- header.php
+++ header.php
@@ -15,7 +15,8 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-
+<link href="<?php echo get_stylesheet_directory_uri("/') . "/inc/boostrap-3.3.7/dist/css/boostrap.css"; ?>" rel="stylesheet">
+<link href="<?php echo get_stylesheet_directory_uri("/') . "/inc/boostrap-3.3.7/dist/css/navbar.css"; ?>" rel="stylesheet">
 <?php wp_head(); ?>
 </head>
 
